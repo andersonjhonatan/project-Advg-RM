fe68c23db5efb82a63d3c7fd861fbb45bddff3b1
Anderson <105141966+andersonjhonatan@users.noreply.github.com>
2024-02-19 13:41:27 -0300

    styles: component about

--- src/components/AboutComponent.tsx
+++ src/components/AboutComponent.tsx
@@ -6,7 +6,36 @@ type Props = {}
 const AboutComponent = (props: Props) => {
   return (
     <div>
-      <Image src='/pexels-katrin-bolovtsova-6077326.jpg' alt='logo' width={500} height={500} className='object-cover w-screen' />
+      <div className="w-full flex justify-center relative">
+        <Image
+          src="/pexels-katrin-bolovtsova-6077326.jpg"
+          alt="logo"
+          width={200}
+          height={100}
+          className="object-cover w-full h-full"
+        />
+
+        <div className="absolute top-0 translate-y-1/2 left-32 w-2/5 h-2/4 bg-black/50 text-white border border-gray-700 gap-11 flex flex-col">
+          <div className="p-8 gap-8 flex flex-col">
+            <h1 className="text-3xl">Sobre</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim quod, hic
+              officiis totam eum neque beatae! Dicta dolorum iure mollitia. Dicta
+              dignissimos nemo aliquid itaque molestias debitis hic provident quos.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim quod, hic
+              officiis totam eum neque beatae! Dicta dolorum iure mollitia. Dicta
+              dignissimos nemo aliquid itaque molestias debitis hic provident quos.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim quod, hic
+              officiis totam eum neque beatae! Dicta dolorum iure mollitia. Dicta
+              dignissimos nemo aliquid itaque molestias debitis hic provident quos.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/HeaderComponent.tsx
+++ src/components/HeaderComponent.tsx
@@ -17,7 +17,7 @@ const HeaderLinks = [
 const HeaderComponent = () => {
   return (
     <header className="w-screen">
-      <div className="flex mx-auto p-8 justify-evenly bg-neutral-100 fixed top-0 left-auto right-auto w-full shadow-sm shadow-neutral-400 z-200 items-center">
+      <div className="flex mx-auto p-8 justify-evenly bg-neutral-100 fixed top-0 left-auto right-auto w-full shadow-sm shadow-neutral-400 z-10 items-center">
         <div className='flex flex-col items-center'>
           <h1 className={`${roboto.className} text-2xl`}>Rosa Muniz</h1>
           <p className={`${roboto.className} text-sm`}>Advocacia</p>
